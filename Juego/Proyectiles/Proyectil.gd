d8e34c02b36944d8692d0b836c1d508b4385cd3d
escobardie <diego_escobar@live.com>
2022-10-15 22:53:38 -0300

    DISPARO PRIMARIO
    -ACCION DE DESTRUIR EL ENEMIGO
    -CREACION DE COLLISION

--- Juego/Proyectiles/Proyectil.gd
+++ Juego/Proyectiles/Proyectil.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 ## ATRIBUTOS
 var velocidad : Vector2 = Vector2.ZERO
-var danio : float
+var danio : float #= 1.0
 
 ## METODOS
 
@@ -15,12 +15,27 @@ func crear (pos:Vector2, dir:float, vel:float, danio_p:int) -> void:
 	#Con el vector velocidad del proyectil pasa algo parecido al movimiento de la nave… tenemos que rotarla en su dirección.
 	velocidad = Vector2(vel, 0).rotated(dir)
 
+# METODOS
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
 	position += velocidad*delta
 
+# METODOS CUSTOM
+func daniar(otro_cuerpo: CollisionObject2D) -> void:
+	#has_method”? Un método que devuelve true o false
+	if otro_cuerpo.has_method("recibir_danio"):
+		otro_cuerpo.recibir_danio(danio)
+	queue_free()
 
+
+## SEÑALES INTERNAS
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
+
+func _on_body_entered(body: Node) -> void:
+	daniar(body)
+
+func _on_area_entered(area: Area2D) -> void:
+	daniar(area)
