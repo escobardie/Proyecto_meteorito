d0a9f0f30112b04b2bc6e87fbd79524834449626
escobardie <diego_escobar@live.com>
2022-10-23 19:09:26 -0300

    ExplosionMeteorito
    -ADD NUEVAS ANIMACIONES

--- Juego/Explosiones/ExplosionMeteorito.gd
+++ Juego/Explosiones/ExplosionMeteorito.gd
@@ -6,7 +6,16 @@ extends Node2D
 
 
 func _ready() -> void:
-	pass 
+	$AnimationPlayer.play(elerig_explosion_random())
+
+
+func elerig_explosion_random() -> String:
+	randomize()
+	var num_anim:int = $AnimationPlayer.get_animation_list().size() - 1
+	var indice_anim_random:int = randi() % num_anim + 1
+	var list_animacion:Array = $AnimationPlayer.get_animation_list()
+	
+	return list_animacion[indice_anim_random]
 
 
 func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
