cbf6554c31a45a9109e115298a611c8f9f2ba509
escobardie <diego_escobar@live.com>
2022-11-04 13:20:35 -0300

    FONDO
    -ADD FONDO

---
+++ Juego/Niveles/Fondo/FondoEstrellas.gd
@@ -0,0 +1,16 @@
+#FondoEstrellas.gd
+tool
+extends ParallaxBackground
+
+export var color_fondo:Color = Color.black
+
+## METODOS
+func _ready() -> void:
+	$ColorRect.color = color_fondo
+
+func _process(delta: float) -> void:
+	if Engine.editor_hint:
+		$ColorRect.color = color_fondo
+
+## METODOS CUSTOM
+
